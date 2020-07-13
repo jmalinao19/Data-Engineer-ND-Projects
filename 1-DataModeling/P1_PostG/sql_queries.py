4a80cac132c6393adb53520a25ea0daa9c4a65a4
jmalinao19 <63438752+jmalinao19@users.noreply.github.com>
2020-07-13 11:41:14 -0500

    Update sql_queries.py
    fix artist_id not null constraint and fix conflict column specification (user_id)

--- 1-DataModeling/P1_PostG/sql_queries.py
+++ 1-DataModeling/P1_PostG/sql_queries.py
@@ -33,7 +33,7 @@ user_table_create = (""" CREATE TABLE IF NOT EXISTS users (
 song_table_create = (""" CREATE TABLE IF NOT EXISTS songs(
     song_id VARCHAR PRIMARY KEY UNIQUE,
     title VARCHAR,
-    artist_id VARCHAR,
+    artist_id VARCHAR NOT NULL,
     year INT,
     duration FLOAT
 );
@@ -68,7 +68,7 @@ songplay_table_insert = ("""
 
 user_table_insert = ("""
     INSERT INTO users (user_id,first_name,last_name, gender, level) 
-    VALUES (%s,%s,%s,%s,%s) ON CONFLICT DO UPDATE SET level = EXCLUDED.level;
+    VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
